306589760c6ac8e503c05f1d359c612eedc813d3
goudai <clannad.key.36@gmail.com>
2024-01-31 09:51:23 +0900

    GitHub Actions を使って修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,5 +37,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
-
 end
