75f93cc51c1c37f054b76623a59ca1933c3a3509
goudai <clannad.key.36@gmail.com>
2024-02-05 23:02:27 +0900

    再修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Articles" do
-  fdescribe "GET /articles" do
+  describe "GET /articles" do
     subject { get(api_v1_articles_path) }
 
     let!(:aaa_article1) { create(:article, updated_at: 1.days.ago) }
@@ -12,32 +12,32 @@ RSpec.describe "Api::V1::Articles" do
       subject
       # binding.pry
       # res = JSON.parse(response.body)
-      res = response.parsed_body
+      @res = response.parsed_body
       expect(response).to have_http_status(:ok)
     end
 
     it "記事が3つできる" do
       subject
-      res = response.parsed_body
-      expect(res.length).to eq 3
+      @res = response.parsed_body
+      expect(@res.length).to eq 3
     end
 
     it "更新順になっている" do
       subject
-      res = response.parsed_body
-      expect(res.map.pluck("id")).to eq [ccc_article3.id, aaa_article1.id, bbb_article2.id]
+      @res = response.parsed_body
+      expect(@res.map.pluck("id")).to eq [ccc_article3.id, aaa_article1.id, bbb_article2.id]
     end
 
     it "返ってくるkeyがid, title, updated_at, userである" do
       subject
-      res = response.parsed_body
-      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
+      @res = response.parsed_body
+      expect(@res[0].keys).to eq ["id", "title", "updated_at", "user"]
     end
 
     it "返ってくるkeyがid, name, emailである" do
       subject
-      res = response.parsed_body
-      expect(res[0]["user"].keys).to eq ["id", "name", "email"]
+      @res = response.parsed_body
+      expect(@res[0]["user"].keys).to eq ["id", "name", "email"]
     end
   end
 end
