ec4a29d78b2139f79a59fade8b629f39d301a2fe
goudai <clannad.key.36@gmail.com>
2024-02-19 00:37:57 +0900

    下書き一覧及び詳細に関する API の実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -10,7 +10,7 @@ module Api::V1
 
     def show
       article = Article.published.find(params[:id])
-      render json: article, each_serializer: Api::V1::ArticleSerializer
+      render json: article, serializer: Api::V1::ArticleSerializer
     end
 
     def create
